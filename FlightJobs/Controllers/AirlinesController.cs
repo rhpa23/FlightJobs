4340ef8cb6cec1a85ab6c7a31b31ae12127ab6d3
Rudolf <rhpa23@gmail.com>
2018-02-17 18:18:17 -0300

    Message bug.

--- FlightJobs/Controllers/AirlinesController.cs
+++ FlightJobs/Controllers/AirlinesController.cs
@@ -48,7 +48,7 @@ namespace FlightJobs.Controllers
             }
             else
             {
-                TempData["Message"] = string.Format("You need {0} scores to sign contract with {0}.", airline.Score, airline.Name);
+                TempData["Message"] = string.Format("You need {0} scores to sign contract with {1}.", airline.Score, airline.Name);
                 return RedirectToAction("Index");
             }
         }
